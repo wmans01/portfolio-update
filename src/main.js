fabef689d541d85d2c4adfd0f81b20f8bfbef96d
wmans01 <wmans0001@gmail.com>
2026-04-29 18:10:10 -0400

    Create Website (Pre-Renders)

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+<section id="center">
+  <h1 id="bold">Jeremy Wang</h1>
+  <h2>Love for all things hardware</h2>
+</section>
+<h5>
+  Github: <a href="https://github.com/wmans01">wmans01</a>
+  LinkedIn: <a href="https://www.linkedin.com/in/jeremy-wang-868a70406/">Jeremy Wang</a>
+  Instagram: <a href="https://www.instagram.com/jeremp0/">@jeremp0</a>
+  Updated: 4/29/2026
+</h5>
+`
